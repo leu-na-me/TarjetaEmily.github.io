30bd215ce14f0b6267c2ed338d0f11c0ad6de21e
Emanuel Jaramillo <107423007+leu-na-me@users.noreply.github.com>
2024-05-02 12:30:34 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function Si(){
+    alert("Muchas gracias, te quiero mucho personita bella💞");
+  }
+  
+  function No(){
+    alert("Oh, esta bien, no hay problema igual quiero recordarte de nuevo de todas formas te quiero mucho💝");
+  }
+  
